82cc6c1dd18f96dd402a601ff34321e7bed371f2
Victormrf <vmrf2000@hotmail.com>
2024-12-22 23:17:28 -0300

    Add nestjs-api and next-frontend as regular folders

---
+++ nestjs-api/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Global, Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MapsModule } from './maps/maps.module';
+import { ConfigModule } from '@nestjs/config';
+import { RoutesModule } from './routes/routes.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Global()
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    MapsModule,
+    RoutesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
